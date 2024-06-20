3e6cc473a6d0e44d8a9e7facda6e97b9d659802c
birundha94 <bindhubecse@gmail.com>
2024-06-20 12:56:33 +0530

    functions class 2

--- demo.js
+++ demo.js
@@ -1,15 +1,42 @@
-var a=3
-var b=3
-console.log(a+b)
-function myname()
-{
-    console.log(" hello")
+//variable
+ let name='birundha';
+console.log(name);
+let firstname='birundha';
+let lastname='s';
+console.log(firstname+lastname); 
+// function declaration
+function sayhi(){
+    console.log('hi all')
+};
+sayhi();
+//functions as expressions
+let expfun=function(){
+    console.log('this is first citizens')
 }
-myname()
-var c=10
-var d =24
-function add()
-{
-    console.log(c+d)
+expfun();
+var a=2;
+var b=3;
+function add(num1,num2){
+    var ans=num1+num2;
+    return ans;
 }
-add()
+let add1=add(a,b);
+let add2=add(5,6);
+console.log(add1);
+console.log(add2);
+
+var n=3;
+function square(num3){
+    var ans1=num3*num3;
+    return ans1;
+}
+let square1=square(n);
+console.log(square1);
+
+let msg='helloo';
+var msgval='helllo world';
+function hoisting(){
+    console.log(msgval,' this is hoisting and functions')
+}
+console.log(msgval);
+hoisting();
