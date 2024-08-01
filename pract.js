0e61022bb6621afb9795206f347127511195324c
birundha94 <bindhubecse@gmail.com>
2024-08-01 13:59:11 +0530

    intro

--- pract.js
+++ pract.js
@@ -15,10 +15,62 @@
 
 //Write a JavaScript program to implement a function that executes a given function repeatedly at a fixed interval using 'setInterval()'.
 
-function setinter(fixinter){
-    setInterval(fixinter,1000);
+// function setinter(fixinter){
+//     setInterval(fixinter,1000);
+// }
+// function fixinter(){
+//     console.log("intervel called");
+// }
+// setinter(fixinter);
+
+// let arr=[1,2,3,4]
+// let abc=[];
+// for(let i=0;i<arr.length;i++){
+//     abc.push(arr[i]);
+
+// }
+// console.log(abc);
+// if(arr===abc){
+//     console.log("true");
+// }else
+// {
+//     console.log("false");
+// }
+// var a=3;
+// var b=3;
+// if(a===b){
+//     console.log("true");
+// }else{
+//     console.log("false");
+// }
+
+
+
+
+// let obj={
+//     name:'arav'
+
+// }
+// let obj2={
+//     name:'arav'
+// }
+// if(obj===obj2){
+//     console.log("true");
+// } else{
+//     console.log("false");
+// }
+
+var a;
+var b= typeof a;
+console.log(b);
+console.log(typeof null)
+var c;
+var d=typeof c;
+if(b===d){
+    console.log(true);
+
+}else
+{
+    console.log(false);
+
 }
-function fixinter(){
-    console.log("intervel called");
-}
-setinter(fixinter);
